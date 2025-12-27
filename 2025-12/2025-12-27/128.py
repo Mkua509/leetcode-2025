3c5000d6cdf72f6912b1147d0943e25e4f6fb83e
Mkua509 <maternuskuang@gmail.com>
2025-12-27 21:55:59 +1300

    Hashing version of 128 dopne

--- 2025-12/2025-12-27/128.py
+++ 2025-12/2025-12-27/128.py
@@ -4,24 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-
-        # If not num essentiall means falsy in a boolean context: If 0, None, False or a empty collection such as list, string etc...
-        if not nums:
-            return 0
-        result = 0
-        nums.sort()
-
-        current = nums[0]
+        new_set = set(nums)
         count = 0
-        i = 0
 
-        while i < len(nums):
-            if current != nums[i]:
-                current = nums[i]
-                count = 0
-            while i < len(nums) and current == nums[i]:
-                i += 1
-            count += 1
-            current += 1
-            result = max(result, count)
-        return result
+        for num in new_set:
+            if (num - 1) not in new_set:
+                length = 1
+                while (num + length) in new_set:
+                    length += 1
+                count = max(length, count)
+        return count
